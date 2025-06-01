387c57b36b01cba819e2d9cc82db05568d2d5826
Muhammad Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2025-06-01 21:31:14 +0700

    feat(03-message-broker): 03-subscribing

--- 03-message-broker/03-subscribing/go.mod
+++ 03-message-broker/03-subscribing/go.mod
@@ -3,3 +3,14 @@ module 03-message-broker/03-subscribing
 go 1.23.0
 
 toolchain go1.24.1
+
+require (
+	github.com/ThreeDotsLabs/watermill v1.4.6 // indirect
+	github.com/ThreeDotsLabs/watermill-redisstream v1.4.3 // indirect
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/lithammer/shortuuid/v3 v3.0.7 // indirect
+	github.com/oklog/ulid v1.3.1 // indirect
+	github.com/redis/go-redis/v9 v9.9.0 // indirect
+)
--- 03-message-broker/03-subscribing/main.go
+++ 03-message-broker/03-subscribing/main.go
@@ -1,5 +1,43 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/ThreeDotsLabs/watermill"
+	"github.com/ThreeDotsLabs/watermill-redisstream/pkg/redisstream"
+	"github.com/ThreeDotsLabs/watermill/message"
+	"github.com/redis/go-redis/v9"
+)
+
 func main() {
+	logger := watermill.NewStdLogger(false, false)
+	rdb := redis.NewClient(&redis.Options{
+		Addr: os.Getenv("REDIS_ADDR"),
+	})
+
+	subscriber, err := redisstream.NewSubscriber(redisstream.SubscriberConfig{
+		Client: rdb,
+	}, logger)
+
+	if err != nil {
+		logger.Error("failed to create subscriber", err, nil)
+		return
+	}
+
+	messages, err := subscriber.Subscribe(context.Background(), "progress")
+	if err != nil {
+		logger.Error("failed to subscribe", err, nil)
+		return
+	}
+
+	for msg := range messages {
+		go func(m *message.Message) {
+			orderID := string(m.Payload)
+			fmt.Printf("Message ID: %s - %s%%\n", m.UUID, orderID)
+			m.Ack()
+		}(msg)
+	}
 
 }
